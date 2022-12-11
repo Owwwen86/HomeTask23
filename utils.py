17eae84c6f816db6323c6ab3ac0587d3fe2c18b5
Owwwen86 <smirnovkirill@mail.ru>
2022-12-11 13:04:10 +0300

    HomeTask23

---
+++ utils.py
@@ -0,0 +1,47 @@
+
+def filter_query(param: str, data: list[str]) -> list[str]:
+    """Search (filter) for given param in data"""
+    return list(filter(lambda row: param in row, data))
+
+
+def map_query(param: str, data: list[str]) -> list[str]:
+    col_number = int(param)
+    return list(map(lambda row: row.split(' ')[col_number], data))
+
+
+def unique_query(data: list[str], *args, **kwargs) -> list[str]:
+    result = []
+    seen = set()
+    for row in data:
+        if row in seen:
+            continue
+        else:
+            result.append(row)
+            seen.add(row)
+    return result
+
+
+def sort_query(param, data: list[str]) -> list[str]:
+    reverse = False if param == 'asc' else True
+    return sorted(data, reverse=reverse)
+
+
+def limit_query(param, data: list[str]) -> list[str]:
+    limit = int(param)
+    return data[:limit]
+
+
+CMD_TO_FUNCTION = {
+    'filter': filter_query,
+    'map': map_query,
+    'unique': unique_query,
+    'sort': sort_query,
+    'limit': limit_query,
+}
+
+
+def build_query(cmd, param, filename, data=None):
+    if not data:
+        with open(f'data/{filename}') as file:
+            data = list(map(lambda row: row.strip(), file))
+    return CMD_TO_FUNCTION[cmd](param=param, data=data)
---
+++ views.py
@@ -0,0 +1,29 @@
+from flask import Blueprint, request, jsonify
+from marshmallow import ValidationError
+
+from schemas import RequestParamListSchema
+from utils import build_query
+
+
+main_bp = Blueprint('main', __name__)
+
+
+@main_bp.route('/perform_query', methods=['POST'])
+def perform_query():
+
+    try:
+        params = RequestParamListSchema().load(request.json)
+    except ValidationError as error:
+        return error.messages, '400'
+
+    result = None
+    for query in params['queries']:
+        result = build_query(
+            cmd=query['cmd'],
+            param=query['value'],
+            filename=params['filename'],
+            data=result,
+        )
+
+    return jsonify(result), '200'
+
